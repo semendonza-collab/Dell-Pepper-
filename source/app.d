18207f1c1a9bb87ff9d396b18979e989b654cc68
Ryan D <ryandoby10@gmail.com>
2026-07-28 21:20:02 -0400

    Added welcome to Dellpeper

--- source/app.d
+++ source/app.d
@@ -2,6 +2,9 @@ import std.stdio;
 
 void print_ui()
 {
+
+  
+  writeln("Welcome to Bellpepers! -h for help");
   writeln("1. Make a task");
   writeln("2. List task's");
   writeln("3. Remove task");
