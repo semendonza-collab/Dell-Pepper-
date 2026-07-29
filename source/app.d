f55f9f7a7f9f5f83bb69b49abb9580f034f0ef8a
Ryan D <ryandoby10@gmail.com>
2026-07-28 21:16:25 -0400

    Added simple menu

--- source/app.d
+++ source/app.d
@@ -1,6 +1,14 @@
 import std.stdio;
 
+void print_ui()
+{
+  writeln("1. Make a task");
+  writeln("2. List task's");
+  writeln("3. Remove task");
+  writeln("4. Exit");
+}
+
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+  print_ui();
 }
