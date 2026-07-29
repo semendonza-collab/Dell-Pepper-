0021258cc7057d3f486b597aebe633c3b628fb9b
Samuel <semendonza@gmail.com>
2026-07-28 22:26:58 -0400

    Update mainapp.d

--- source/mainapp.d
+++ source/mainapp.d
@@ -1,31 +1,42 @@
 module mainapp;
-
 import std.stdio;
 import  std.array;
 
 void main()
 {
- bool listing = 0;
- bool isListing;
+ 
+ bool listing = 0;                                        // everything here is important dont get rid of anything or i will chop my penis off and shove it up youre ass
+ bool isListing;             
 
  string UserNote; 
  readfln(UserNote);
 
- int[] list = new list[1, 2, 3, 4];
- 
+ int[] list = [1, 2, 3, 7];
+
+ for(1; 1 < 100; i++)
+ {
+  list[];
+  //double.alignof(destroy);
+ }
+
  if(UserNote == "list" || "List")
  {
-   listing = 1;
-   if(listing = 1)
+   listing = 1; 
+
+   if(listing == 1)
    {
     isListing = true;
    }
- }
+   else {
+     isListing = false;
+   }
 
+ }
  if(listing)
  {
-  writeln(list.array(2));
- } else 
+  writeln(list);
+ } 
+ else 
  {
    writeln("Please use the !Help command.");
  }
