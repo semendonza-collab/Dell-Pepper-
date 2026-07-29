4f6c19697680865af7e32e6b3907d59784a9e4d4
Samuel <semendonza@gmail.com>
2026-07-28 21:36:25 -0400

    working
    I made stuff work

--- source/app.d
+++ source/app.d
@@ -3,4 +3,22 @@ import std.stdio;
 void main()
 {
 	writeln("Edit source/app.d to start your project.");
+
+	//string[] list = [1, 2, 3, 4,
+	string[] list = [ "Doggy", "Kitty", "Whore", "Pussy"];
+
+    string UserNote; 
+    readfln(UserNote);
+    writefln(UserNote);
+
+	if(UserNote == "list" || "List")
+	{
+		writeln(list); //put array here
+	}
+	else
+	{
+		writeln("Invalid command");
+	}
+
+	//return 1; 
 }
